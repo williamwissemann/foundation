885bd44d2f66e4041af4553e6f1be54cc9ef7216
William Wissemann <williamwissemann@gmail.com>
2021-02-16 17:00:50 -0500

    Allow for setup.py to resolve packages loaded in from a local directory

--- artifacts/cookiecutter/template/{{cookiecutter.package_name}}/setup.py
+++ artifacts/cookiecutter/template/{{cookiecutter.package_name}}/setup.py
@@ -26,20 +26,36 @@ with open('README.rst', 'r') as f:
 with open('CHANGELOG.rst', 'r') as f:
     changes = f.read()
 
-
 def parse_requirements(filename):
     ''' Load requirements from a pip requirements file '''
     with open(filename, 'r') as fd:
+        print(f"resolve packages dependencies in {filename}")
         lines = []
         for line in fd:
-            line.strip()
-            if line and not line.startswith("#"):
-                lines.append(line)
+            line = line.strip()
+            potential_path_obj = line.split('#')
+            potential_path = potential_path_obj[0].strip()
+  
+            if os.path.isdir(potential_path):
+                """ the folder name needs to match the package name for this to work """
+                package_folder = pathlib.PurePath(potential_path)
+                package = f'{package_folder.name}'
+                if len(potential_path_obj) > 1:
+                    version_pin = potential_path_obj[1].strip()
+                    package = f'{package}{version_pin}'
+                    print(f' - resolved path to package' 
+                                 f'\'{potential_path}\' --> {package} added')
+                else:
+                    print(f' - resolved path to package' 
+                                 f'\'{potential_path}\' --> {package} (latest) added')
+                lines.append(package)
+            elif line and not line.startswith("#"):
+                print(f' - package \'{potential_path}\' added')
+                lines.append(line.strip())
     return lines
 
-
-requirements = parse_requirements('requirements.txt')
-
+requirements = parse_requirements('requirements.local.txt')
+requirements.extend(parse_requirements('requirements.txt'))
 
 if __name__ == '__main__':
     setup(
